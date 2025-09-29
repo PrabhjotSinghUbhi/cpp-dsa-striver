4123df3f9d34c6aa49bb533c3f52bc52fbbda9d8
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-09-29 15:50:59 +0530

    Implement function to convert string to lowercase and remove whitespace

--- temp.py
+++ temp.py
@@ -1,1 +1,6 @@
-convert string to lowerspace and remove whitespace
+def to_lower_no_whitespace(s):
+	return ''.join(s.lower().split())
+
+if __name__ == "__main__":
+	s = input("Enter a string: ")
+	print(to_lower_no_whitespace(s))
